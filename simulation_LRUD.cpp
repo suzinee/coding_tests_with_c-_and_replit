d1de860aad660a53bcf66cf598169ca722076e9b
suzinee <ggaebong.u@gmail.com>
2020-10-18 14:36:40 +0000

    rewrite the code

--- simulation_LRUD.cpp
+++ simulation_LRUD.cpp
@@ -4,32 +4,35 @@ using namespace std;
 
 int n;
 string plans;
-int x =1, y=1;
+int x = 1, y = 1;
 
+//L R U D
 int dx[4] = {0, 0, -1, 1};
 int dy[4] = {-1, 1, 0, 0};
-char moveTypes[4] = {'L','R','U','D'};
+char move_types[4] = {'L','R','U','D'};
 
-int simulation_LRUD(void)
+int simulation_LRUD()
 {
-  cin >> n;
-  cin.ignore();
-  getline(cin, plans);
-
-  for(int i =0; i<plans.size(); i++){
-    char plan = plans[i];
-    int nx = -1, ny = -1;
-    for (int j = 0; j<4; j++){
-      if(plan == moveTypes[j]){
-        nx = x+dx[j];
-        ny = y+dy[j];
-      }
+    cin >> n;
+    cin.ignore();
+    getline(cin, plans);
+    
+    for(int i = 0; i < plans.size(); i++)
+    {
+        int nx = -1, ny = -1;
+        for(int j = 0; j < sizeof(move_types); j++)
+        {
+            if(plans[i] == move_types[j])
+            {
+                nx = x + dx[j];
+                ny = y + dy[j];
+            }
+        }
+        if(nx < 1 || ny < 1 || nx > n || ny > n)
+            continue;
+        x = nx;
+        y = ny;
     }
-    if(nx <1 || ny <1 || nx >n || ny > n)
-      continue;
-    x = nx;
-    y = ny;
-  }
-  cout << x << ' ' << y << '\n';
-  return 0;
+    cout << x << ' ' << y << '\n';
+    return 0;
 }
